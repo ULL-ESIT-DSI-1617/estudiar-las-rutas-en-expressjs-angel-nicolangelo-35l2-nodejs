8b8151d8a7f5e230b393a3e487a4e82fe446b0a1
nico_apache <nico10907@hotmail.com>
2017-03-01 16:54:32 +0000

    añadido metodo post

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var app = express();
 var port = 5000;
 
 app.get('/', function (req, res) {
-  res.send('Hola mundo5000!!');
-})
+  res.send('Got a GET request');
+});
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
 
 app.listen(process.env.PORT, process.env.IP,function () {
   console.log('Ejemplo de app escuchando por el puerto', port);
