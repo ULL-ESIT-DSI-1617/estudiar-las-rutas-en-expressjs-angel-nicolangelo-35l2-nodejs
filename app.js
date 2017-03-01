319506aaa1c75bdb7d0a1bcd5326447ffa2524cc
nico_apache <nico10907@hotmail.com>
2017-03-01 10:56:41 +0000

    mejorado app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express')
-var app = express()
-var port = 5000
+var express = require('express');
+var app = express();
+var port = 5000;
 
 app.get('/', function (req, res) {
-  res.send('Hola mundo!!')
+  res.send('Hola mundo5000!!');
 })
 
-app.listen(port, function () {
-  console.log('Ejemplo de app escuchando por el puerto', port)
-})
-
+app.listen(process.env.PORT, process.env.IP,function () {
+  console.log('Ejemplo de app escuchando por el puerto', port);
+});
+// app.listen(port, function () {
+//   console.log('Ejemplo de app escuchando por el puerto', port);
+// })
