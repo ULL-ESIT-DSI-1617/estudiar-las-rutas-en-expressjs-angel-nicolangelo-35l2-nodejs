f192b629ed65c625ac9cf02913391fa3069c371d
nico_apache <nico10907@hotmail.com>
2017-03-02 13:19:25 +0000

    añadido cookie

--- src/cookieparser.js
+++ src/cookieparser.js
@@ -13,8 +13,3 @@ app.get('/', function (req, res) {
   // Cookies that have been signed
   console.log('Signed Cookies: ', req.signedCookies);
 });
-
-app.listen(8080);
-
-// curl command that sends an HTTP request with two cookies
-// curl http://127.0.0.1:8080 --cookie "Cho=Kim;Greet=Hello"
