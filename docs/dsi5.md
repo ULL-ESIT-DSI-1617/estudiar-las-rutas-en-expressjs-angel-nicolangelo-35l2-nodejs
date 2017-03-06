9a7db4c855e04f0a33e4598c1165477bcf631385
Alexandra <alexandraesplugas07@gmail.com>
2017-03-06 09:32:08 +0000

    Update 2017-03-06T09:32:06.316Z

--- docs/dsi5.md
+++
@@ -1,4 +0,0 @@
-#Install ExpressJS
-
-![INSTALACIÓN1](install-expressjs1.png)
-![INSTALACIÓN2](node-app-run.png)
--- src/static.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-app.listen(process.env.PORT, process.env.IP);
-app.set('port', 3000);
-// app.use(express.static('public'));
-app.use('/static', express.static('public'));
-
